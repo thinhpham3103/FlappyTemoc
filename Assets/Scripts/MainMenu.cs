16bd013fa0ece7a57f0ca53f2c35cc4d75c8718c
Thinh Pham <61043275+thinhpham3103@users.noreply.github.com>
2023-04-19 21:42:00 -0500

    update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
 
     public void play() {
-        SceneManager.LoadScene("GameScene",LoadSceneMode.Single); // loads next scene (play)
+        SceneManager.LoadScene("GameScene",LoadSceneMode.Single); // loads game scene(play)
     }
     
 }
